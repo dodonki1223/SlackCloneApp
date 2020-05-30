fdaeaf00f60b7a0ea1ea4d207f6ded82a4f257a2
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-05-30 23:46:04 +0900

    :recycle: 1.Slack風UIを作ろう！ - メッセージをわかりやすいようにComponentsに分割しリファクタする

--- pages/index.vue
+++ pages/index.vue
@@ -4,65 +4,19 @@
     以下のコードは読まないと意味不明です……のちにリファクタする
   -->
   <div class="container">
-    <div class="chats-container">
-
-      <div class="chat-container">
-        <div class="thumbnail-container">
-          <img src="https://pbs.twimg.com/profile_images/1131882065337602049/0JRUGd3S_400x400.jpg" />
-        </div>
-        <div class="message-container">
-          <div class="user-name">cohki0305</div>
-          <div class="message">今日も良い天気ですね</div>
-        </div>
-      </div>
-
-      <div class="chat-container">
-        <div class="thumbnail-container">
-          <img src="https://pbs.twimg.com/profile_images/1131882065337602049/0JRUGd3S_400x400.jpg" />
-        </div>
-        <div class="message-container">
-          <div class="user-name">cohki0305</div>
-          <div class="message">そうですねー</div>
-        </div>
-      </div>
-
-    </div>
+    <messages />
   </div>
 </template>
 
 <script>
+import Messages from '~/components/Messages.vue'
 
 export default {
   components: {
+    Messages
   }
 }
 </script>
 
-// scoped はここで書いた css は他の .vue ファイルに一切影響を与えません
-// index.vue にしか適用されないため他のファイルの css が邪魔してきてスタイルがぐちゃぐちゃ
-// になることがないのです。これがコンポーネントの魅力の１つです
-<style scoped>
-.chats-container {
-  padding: 16px;
-}
-
-.chat-container {
-  display: flex;
-  padding: 8px;
-}
-
-.thumbnail-container {
-  margin-right: 16px;
-}
-
-.thumbnail-container img {
-  width: 50px;
-  height: 50px;
-  border-radius: 10px;
-}
-
-.user-name {
-  font-weight: bold;
-}
-
+<style>
 </style>
