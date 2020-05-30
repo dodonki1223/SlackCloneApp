fd93925c4f9004088df6e2f70dd990780b52c4a7
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-05-31 01:07:36 +0900

    :recycle: 2.Vue の基本を学びながらアプリに動きをつける！ - Messageの情報をdataから取得するように修正

--- components/Message.vue
+++ components/Message.vue
@@ -1,14 +1,56 @@
 <template>
   <div class="chat-container">
     <div class="thumbnail-container">
-      <img src="https://pbs.twimg.com/profile_images/1131882065337602049/0JRUGd3S_400x400.jpg" />
+      <img :src="message.user.thumbnail" />
     </div>
     <div class="message-container">
-      <div class="user-name">cohki0305</div>
-      <div class="message">今日も良い天気ですね</div>
+      <div class="user-name">{{ displayName }}</div>
+      <div class="message">{{ message.text }}</div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  // ----------------------------------------------------
+  // 以下の記法はJavaScriptの特殊なものです
+  // {
+  //   data() {
+  //   }
+  // }
+  // ----------------------------------------------------
+  // これは以下と同じです
+  // ----------------------------------------------------
+  // {
+  //   data: () => {
+  //   }
+  // }
+  // ----------------------------------------------------
+  // {
+  //   data: function () {
+  //   }
+  // }
+  // ----------------------------------------------------
+  data() {
+    return {
+      message: {
+        text: '今日も良い天気ですね',
+        user: {
+          name: 'note',
+          thumbnail: 'https://pbs.twimg.com/profile_images/1131882065337602049/0JRUGd3S_400x400.jpg'
+        }
+      }
+    }
+  },
+  computed: {
+    displayName() {
+      return "@" + this.message.user.name
+      // 下記のように書くこともできる
+      // return `@${this.message.user.name}`
+    }
+  }
+}
+</script>
 
 // scoped はここで書いた css は他の .vue ファイルに一切影響を与えません
 // index.vue にしか適用されないため他のファイルの css が邪魔してきてスタイルがぐちゃぐちゃ
