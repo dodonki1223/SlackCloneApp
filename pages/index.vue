e21802b2da898c78db540de4005f3c49c318370d
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-05-31 15:07:13 +0900

    :sparkles: 3.データベースの値を取得してチャンネルを作ろう - 3-2.Firstoreを使ってみよう 作成したFirestoreのデータが呼べるか確認する

--- pages/index.vue
+++ pages/index.vue
@@ -18,11 +18,19 @@
 <script>
 import Messages from '~/components/Messages.vue'
 import ChatForm from '~/components/ChatForm.vue'
+import { db } from '~/plugins/firebase'
 
 export default {
   components: {
     Messages,
     ChatForm
+  },
+  mounted() {
+    db.collection('channels').doc('KUZyb5lVSJtvJfGbTKrH').get()
+      .then((doc) => {
+        console.log('doc.id: ', doc.id)
+        console.log('doc.data(): ', doc.data())
+      })
   }
 }
 </script>
